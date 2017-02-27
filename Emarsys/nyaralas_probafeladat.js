4cfc6d9deba7ff2f3bd7aacd7a9da27ac4743f26
bereczb <balazs.berecz@gmail.com>
2017-02-27 23:34:59 +0100

    completely modified

--- Emarsys/nyaralas_probafeladat.js
+++ Emarsys/nyaralas_probafeladat.js
@@ -1,45 +1,67 @@
 'use strict';
 
-var targets = [['u', 0], ['v', 0], ['w', 0], ['x', 0], ['y', 0], ['z', 0]];
-var targets2 = [['u', 0], ['v', 'w'], ['w', 'z'], ['x', 'u'], ['y', 'v'], ['z', 0]];
+var targets1 = [['u', 0], ['v', 0], ['w', 0], ['x', 0], ['y', 0], ['z', 0]];
+var targets2 = [['u', 0], ['v', 'w'], ['w', 'z'], ['x', 'u'], ['y', 'v'], ['z', 'u']];
+var targets3 = [['u', 0], ['v', 'w'], ['w', 0], ['x', 'y'], ['y', 'z'], ['z', 'x']];
+var targets4 = [['u', 'a'], ['v', 'w'], ['w', 'z'], ['x', 'u'], ['y', 'v'], ['z', 'u']];
 
 function routeOptimum(inputTargets) {
 
   var result = [];
-  var dependencies = [];
+  var targets = [];
+  var targetsWithDependencies = [];
   var inputLength = inputTargets.length;
 
+  // Error handling - duplicated target
   for (let i = 0; i < inputLength; i++) {
+    if (targets.indexOf(inputTargets[i][0]) === -1) {
+      targets.push(inputTargets[i][0]);
+    } else {
+      return 'input error: duplicated target';
+    }
+  }
 
-    if (result.indexOf(inputTargets[i][0]) === -1) {
+  // separating targets with and without dependencies
+  for (let i = 0; i < inputLength; i++) {
+    if (inputTargets[i][1] == false) {
       result.push(inputTargets[i][0]);
     } else {
-      return 'input error: repeated target'
-    }
-
-    if (dependencies.indexOf(inputTargets[i][1]) != -1) {
-      return 'input error: repeated dependency'
-    } else if (inputTargets[i][1]) {
-      dependencies.push(inputTargets[i][1]);
+      targetsWithDependencies.push(inputTargets[i]);
     }
   }
 
-  for (let i = 0; i < inputLength; i++) {
+  // Error handling - we need at lest one target without dependency
+  if (result.length == 0) {
+    return 'input error: missing entry point';
+  }
 
-    if (inputTargets[i][1] != false) {
-      let targetPos = result.indexOf(inputTargets[i][0]);
-      let dependencyPos = result.indexOf(inputTargets[i][1]);
-      if (targetPos < dependencyPos) {
-        result.splice(targetPos, 0, inputTargets[i][1]);
-        result.splice(dependencyPos + 1, 1);
+  // filling up result variable with the rest of the targets
+  while (targetsWithDependencies.length > 0) {
+
+    let lastRound = true;
+
+    for (let i = 0; i < targetsWithDependencies.length; i++) {
+      if (result.includes(targetsWithDependencies[i][1])) {
+        result.push(targetsWithDependencies[i][0]);
+        targetsWithDependencies[i][1] = 0;
+        lastRound = false;
       }
     }
-  }
 
-  return result
+    if (lastRound) {
+      return 'input error: circular dependencies';
+    }
+
+    targetsWithDependencies = targetsWithDependencies.filter(function(e){
+      return e[1];
+    });
+  }
+  return result;
 }
 
-console.log(routeOptimum(targets));
+console.log(routeOptimum(targets1));
 console.log(routeOptimum(targets2));
+console.log(routeOptimum(targets3));
+console.log(routeOptimum(targets4));
 
 module.exports = routeOptimum;
